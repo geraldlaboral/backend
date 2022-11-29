78bef66895d0654a0aadfe45779707ea86744bfb
geraldlaboral <113404380+geraldlaboral@users.noreply.github.com>
2022-11-29 16:53:26 -0600

    Add files via upload

---
+++ models/usuario.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const schemaUsuario = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    nacimiento: { type: Date, required: true },
+    estatura: { type: Number, required: true },
+    genero: { type: String, required: true },
+    correo: { type: String, required: true, unique: true },
+    foto: { type: String, required: false },
+    pesoMeta: { type: Number, required: true }
+});
+module.exports = mongoose.model('Usuario', schemaUsuario, 'usuario');
