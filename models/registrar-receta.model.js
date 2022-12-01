43c216d51d76a11fb76212600093df16e0d7f6dc
Andonex <andony1896@gmail.com>
2022-12-01 17:07:26 -0600

    Update registrar-receta.model.js

--- models/registrar-receta.model.js
+++ models/registrar-receta.model.js
@@ -1,14 +1,14 @@
 "use strict"
 
 const mongoose = require("mongoose");
-const schemaRecetas = new mongoose.Schema({
+const schemaReceta = new mongoose.Schema({
     nombre: { type: String, required: true, unique: true },
     ingredientes: { type: String, required: true, unique: false },
     imagen: { type: String, required: false, unique: false },
-    pasos_de_la_receta: { type: String, required: true, unique: false },
-    tipo_de_comida: { type: String, required: true, unique: false },
-    categoria_de_comida: { type: String, required: true, unique: false }
+    pasosReceta: { type: String, required: true, unique: false },
+    tipoComida: { type: String, required: true, unique: false },
+    categoriaComida: { type: String, required: true, unique: false }
 
 });
 
-module.exports = mongoose.model("Receta", schemaRecetas, "recetas");
+module.exports = mongoose.model("Receta", schemaReceta, "recetas");
