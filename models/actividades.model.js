1e67472531789e60315f28d9b826c0ee9cab7d5f
AndresV29 <andresvillalobos.carballo920@gmail.com>
2022-12-11 20:06:24 -0600

    arreglos en la parte de actividades

--- models/actividades.model.js
+++ models/actividades.model.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-let schemaActividad = new mongoose.Schema({
-    'horaInicio': { type: Number, required: true, unique: false },
-    'horaFinal': { type: Number, required: true, unique: false },
-    'fecha': { type: Number, required: true, unique: false },
-    'nombre': { type: String, required: true, unique: false },
-    'tipo': { type: String, required: true, unique: false },
-    'peso': { type: Number, required: true, unique: false },
-    'imc': { type: Number, required: true, unique: false }
+const mongoose = require("mongoose");
+const schemaActividad = new mongoose.Schema({
+    horaInicio: { type: String, required: true, unique: false },
+    horaFinal: { type: String, required: true, unique: false },
+    fecha: { type: String, required: false, unique: false },
+    nombre: { type: String, required: true, unique: true },
+    tipo: { type: String, required: true, unique: false },
+    peso: { type: String, required: false, unique: false },
+    imc: { type: String, required: false, unique: false }
 });
 module.exports = mongoose.model('Actividad', schemaActividad, 'actividades');
