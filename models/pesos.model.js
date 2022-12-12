065e30bc281793455191b84096a1200c9ad8bef3
fschmidtr <118025239+fschmidtr@users.noreply.github.com>
2022-12-11 21:43:41 -0600

    Update pesos.model.js

--- models/pesos.model.js
+++ models/pesos.model.js
@@ -5,5 +5,7 @@ const schemaPesos = new mongoose.Schema({
     fecha: { type: Date, required: true, unique: true },
     peso: { type: Number, required: true },
     estatura: { type: Number, required: true },
+    imc: { type: Number, required: true },
+    clasificacion: { type: String, required: true },
 });
 module.exports = mongoose.model("Pesos", schemaPesos, "pesos");
