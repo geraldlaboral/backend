24c31c4a566d5adac4b3646e792658e9f15cfe2d
geraldlaboral <113404380+geraldlaboral@users.noreply.github.com>
2022-12-12 18:35:43 -0600

    Update pesos.model.js

--- models/pesos.model.js
+++ models/pesos.model.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const schemaPesos = new mongoose.Schema({
     fecha: { type: Date, required: true, unique: true },
     peso: { type: Number, required: true },
-    estatura: { type: Number, required: true },
+    estatura: { type: Number, required: true }
     // imc: { type: Number, required: true },
     // clasificacion: { type: String, required: true },
 });
